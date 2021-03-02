b24e6456b2ce76b08fa44cd351c0cf10cd8d4790
Million900o <apaolini900o@gmail.com>
2021-03-02 12:34:25 +0000

    changing code

--- index.js
+++ index.js
@@ -26,24 +26,57 @@ const client = new Discord.Client({
   }
 })
 
-// Date function for logs
-function getDate () {
+/**
+ * Date function for logs
+ */
+function getDate() {
   const date = new Date()
   return colors.cyan(`${date.toLocaleTimeString()} : ${date.toLocaleDateString()}`)
 }
 
-// Format the logs nicely
-function log (...args) {
+/**
+ * Format the logs nicely
+ * @param  {...any} args The arguments to be logged
+ */
+function log(...args) {
   const msg = args.map(arg => colors.bold(arg))
   console.log(getDate(), '|', ...msg)
 }
 
-// Fancy, right?
-client.on('ready', () => {
-  log(`Ready as ${client.user.tag} (${client.user.id})`)
-})
+/**
+ * Coconut mall a channel
+ * @param {Discord.Channel} channel 
+ */
+function coconutMall(channel) {
+  // Join the channel and play it
+  channel.join().then(connection => {
+    const dispatcher = connection.play(PATH, { volume: 0.1 })
 
-function runShit () {
+    // Success!!!!!
+    log(colors.green(`Coconut mall'd ${channel.guild.id}`))
+
+    // When it finishes
+    dispatcher.on('finish', () => {
+      channel.leave()
+    })
+
+    // Disconnect on done
+    dispatcher.on('disconnect', () => {
+      dispatcher.destroy()
+    })
+
+    // Idk some errors ig
+    dispatcher.on('error', (e) => {
+      channel.leave()
+      log(colors.red(e.toString()))
+    })
+  })
+}
+
+/**
+ * Grab all of the channels
+ */
+function runShit() {
   // Get all channels
   // Rules:
   //  * No channels can be of the same guild
@@ -63,45 +96,33 @@ function runShit () {
 
   // Loop through all channels like a boss
   for (const channel of channels) {
-    // Join the channel and play it
-    channel.join().then(connection => {
-      const dispatcher = connection.play(PATH, { volume: 0.1 })
-
-      // Success!!!!!
-      log(colors.green(`Coconut mall'd ${channel.guild.id}`))
-
-      // When it finishes
-      dispatcher.on('finish', () => {
-        channel.leave()
-      })
-
-      // Disconnect on done
-      dispatcher.on('disconnect', () => {
-        dispatcher.destroy()
-      })
-
-      // Idk some errors ig
-      dispatcher.on('error', (e) => {
-        channel.leave()
-        log(colors.red(e.toString()))
-      })
-    })
+    coconutMall(channel)
   }
 }
+
+// Fancy, right?
+client.on('ready', () => {
+  log(`Ready as ${client.user.tag} (${client.user.id})`)
+})
 
 setInterval(runShit, TIME)
 
 // The one command I have because useful
 client.on('message', async (msg) => {
-  if (msg.author.id !== '277183033344524288') return
-
-  if (msg.content === 'cm!force') return runShit()
+  if (msg.content === 'cm!force') {
+    if (msg.member.voice.channel) {
+      coconutMall(msg.member.voice.channel);
+      return msg.channel.send('You must be in a voice channel');;
+    } else return msg.channel.send('You must be in a voice channel');
+  }
 
   if (msg.content === 'cm!mem') {
     return msg.channel.send(`\`\`\`js
-${require('util').inspect(Object.entries(process.memoryUsage()).reduce((T, [K, V]) => (T[K] = (V / (1024 ** 2)).toFixed(1) + 'MB', T), {}))}
-\`\`\``)
+    ${require('util').inspect(Object.entries(process.memoryUsage()).reduce((T, [K, V]) => (T[K] = (V / (1024 ** 2)).toFixed(1) + 'MB', T), {}))}
+    \`\`\``)
   }
+
+  if (msg.author.id !== '277183033344524288') return
 
   if (msg.content.startsWith('cm!eval')) {
     try {
@@ -111,6 +132,11 @@ ${require('util').inspect(Object.entries(process.memoryUsage()).reduce((T, [K, V
     } catch (e) {
       msg.send('```\n' + e.toString() + '\n```')
     }
+  }
+
+  if (msg.content === 'cm!forceall') {
+    runShit()
+    return msg.channel.send('Coconut malling all channels!');
   }
 })
 
